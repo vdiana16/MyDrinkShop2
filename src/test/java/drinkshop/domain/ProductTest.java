e91d6fcb9ba5cf5dc459f04577388b75650affd4
vvssUBB <maria.chisalita@ubbcluj.ro>
2026-03-30 20:25:56 +0300

    surefire

--- src/test/java/drinkshop/domain/ProductTest.java
+++ src/test/java/drinkshop/domain/ProductTest.java
@@ -21,56 +21,56 @@ class ProductTest {
     }
 
     @Test
-    void testGetId() {
+    public void getId() {
         assert 100 == product.getId();
     }
 
     @Test
-    void testGetNume() {
+    public void testGetNume() {
         assert "Limonada".equals(product.getNume());
     }
 
     @Test
-    void testGetPret() {
+    public void testGetPret() {
         assert 10.0 == product.getPret();
     }
 
     @Test
-    void testGetCategorie() {
+    public void testGetCategorie() {
         assert CategorieBautura.JUICE.equals(product.getCategorie());
     }
 
     @Test
-    void testSetCategorie() {
+    public void testSetCategorie() {
         product.setCategorie(CategorieBautura.SMOOTHIE);
         assert CategorieBautura.SMOOTHIE.equals(product.getCategorie());
     }
 
     @Test
-    void testGetTip() {
+    public void testGetTip() {
         assert TipBautura.WATER_BASED.equals(product.getTip());
     }
 
     @Test
-    void testSetTip() {
+    public void testSetTip() {
         product.setTip(TipBautura.BASIC);
         assert TipBautura.BASIC.equals(product.getTip());
     }
 
     @Test
-    void testSetNume() {
+    public void testSetNume() {
         product.setNume("newLimonada");
         assert "newLimonada".equals(product.getNume());
     }
 
     @Test
-    void testSetPret() {
+    public void testSetPret() {
         product.setPret(10.05);
         assert 10.05 == product.getPret();
     }
 
     @Test
-    void testToString() {
+    public void testToString() {
         System.out.println(product.toString());
         assert "Limonada (JUICE, WATER_BASED) - 10.0 lei".equals(product.toString());
     }
