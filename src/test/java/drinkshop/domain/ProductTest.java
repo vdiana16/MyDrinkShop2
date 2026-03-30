424bce82901a35f1806aa91ee88f77f55728c0f0
vvssUBB <maria.chisalita@ubbcluj.ro>
2026-03-30 22:16:50 +0300

    surefire

--- src/test/java/drinkshop/domain/ProductTest.java
+++ src/test/java/drinkshop/domain/ProductTest.java
@@ -6,22 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ProductTest {
+class ProductTest {
 
     Product product;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         product =new Product(100, "Limonada", 10.0, CategorieBautura.JUICE, TipBautura.WATER_BASED);
     }
 
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         product = null;
     }
 
     @Test
-    public void testGetId() {
+    void testGetId() {
         assert 100 == product.getId();
     }
 
